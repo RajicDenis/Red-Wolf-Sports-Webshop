0a83e0a15bc290b1b5c653717a6445b687fac259
RajicDenis <denis.rajic92@gmail.com>
2018-02-21 18:09:24 +0100

    moving CSS

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,77 @@
+<div class="section flex-c">
+            
+    <h1>Subscribe to our Newsletter!</h1>
+
+    <p>Get the latest news about the newest football gear!</p>
+
+    <form id="newsletter-form">
+        <div class="form-box">
+            <input class="input-fld" type="email" name="email" required>
+
+            <button type="submit" class="newsletter-btn"><span class="newsletter-btn-txt">Sign Up</span><i class="fa fa-angle-double-right black ml"></i></button>
+        </div>
+    </form>
+
+</div>
+
+
+<div class="footer">
+    
+    <div class="footer-logo"><img class="fl-img" src="{{URL::asset('images/redwolf.png')}}"></div>
+
+    <div class="footer-links-box">
+        
+        <div class="link-box">
+            <h4 class="white ul">Red Wolf Sports</h4>
+
+            <ul class="footer-list d-flex">
+                <li class="mb-2">ABOUT US</li>
+                <li class="mb-2">LOCATION</li>
+                <li class="mb-2">INFO</li>
+            </ul>
+        </div>
+
+        <div class="link-box">
+            <h4 class="white ul">Service</h4>
+
+            <ul class="footer-list d-flex">
+                <li class="mb-2">CONTACT US</li>
+                <li class="mb-2">COMPLAINT</li>
+                <li class="mb-2">SITEMAP</li>
+            </ul>
+        </div>
+
+        <div class="link-box">
+            <h4 class="white ul">Conditions</h4>
+
+            <ul class="footer-list d-flex">
+                <li class="mb-2">FREIGHT POLICY</li>
+                <li class="mb-2">RETURN POLICY</li>
+                <li class="mb-2">PAYMENT</li>
+            </ul>
+        </div>
+
+        <div class="link-box">
+            <h4 class="white ul">Privacy Policy</h4>
+
+            <ul class="footer-list d-flex">
+                <li class="mb-2">PRIVACY</li>
+                <li class="mb-2">COOKIES</li>
+            </ul>
+        </div>
+
+    </div>
+
+    <div class="footer-social d-flex align-items-center mt-3">
+        <h4 class="white mr-4">Follow Us:</h4>
+
+        <div class="soc-btn"><i class="fab fa-facebook-f white"></i></div>
+
+        <div class="soc-btn"><i class="fab fa-twitter white"></i></div>
+
+        <div class="soc-btn"><i class="fab fa-instagram white"></i></div>
+
+        <div class="soc-btn"><i class="fab fa-linkedin-in white"></i></div>
+    </div>
+
+</div>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,20 @@
+<div class="header-nav">
+    <div class="nav">
+        <ul class="list">
+            <li><i class="fas fa-bars white"></i></li>
+            <li>ABOUT US</li>
+            <li>CONTACT US</li>
+            <li>PRODUCTS</li>
+        </ul>
+    </div>
+
+    <div class="logo"><img class="logo-img" src="{{URL::asset('images/redwolf.png')}}"></div>
+
+   <div class="nav2">
+        <ul class="list">
+            <li class="log-li"><div class="icon-box"><i class="far fa-user-circle s2x white"></i></div> Log In</li>
+            <li class="log-li"><div class="icon-box"><i class="fas fa-sign-in-alt s2x white"></i></div> Sign Up</li>
+            <li><div class="icon-box"><i class="fas fa-cart-arrow-down white"></i></div></li>
+        </ul>
+    </div> 
+</div>
